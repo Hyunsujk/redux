2b971cb32d39364304862ee797dd2751c6a00d2a
Hyunsu Kim <hkim@bardavon.com>
2020-09-17 15:09:49 -0500

    created charts and imported to chat window

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,6 +1,7 @@
 import React from "react";
 import store from "../store";
 import Header from "../components/Header";
+import Charts from "../components/Charts";
 
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
@@ -9,6 +10,7 @@ const ChatWindow = ({ activeUserId }) => {
   return (
     <div className="ChatWindow">
       <Header user={activeUser} />
+      <Charts />
     </div>
   );
 };
