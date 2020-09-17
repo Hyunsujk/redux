45959f1c15865fa1e4e89edc3b83068a3142a6bf
Hyunsu Kim <hkim@bardavon.com>
2020-09-17 15:13:02 -0500

    fixed file name and passes active messages to chats component

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,16 +1,18 @@
 import React from "react";
 import store from "../store";
 import Header from "../components/Header";
-import Charts from "../components/Charts";
+import _ from "lodash";
+import Chats from "./Chats";
 
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
   const activeUser = state.contacts[activeUserId];
+  const activeMsgs = state.messages[activeUserId];
 
   return (
     <div className="ChatWindow">
       <Header user={activeUser} />
-      <Charts />
+      <Chats messages={_.values(activeMsgs)} />
     </div>
   );
 };
